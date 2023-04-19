49bc7c0de14d06e754c5f6b69c38cc2e922a7abc
user2023111 <131252108+user2023111@users.noreply.github.com>
2023-04-19 17:36:01 +0800

    Update more.tsx

--- components/pagination/demo/more.tsx
+++ components/pagination/demo/more.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Pagination } from 'antd';
 
-const App: React.FC = () => <Pagination defaultCurrent={6} total={500} />;
+const App: React.FC = () => <Pagination defaultCurrent={6} total={300} />;
 
 export default App;
